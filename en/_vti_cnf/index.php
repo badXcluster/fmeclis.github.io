16e4ab3294e29ce90967ca776fbbb8e3d628c9ae
Fatih Meclis <fatihmeclis@gmail.com>
2025-10-11 23:29:40 +0300

    add site files

---
+++ en/_vti_cnf/index.php
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|09 Dec 2008 15:00:16 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{DF7E026E-FAC4-4DC0-9D33-3DA3D5580666}
+vti_backlinkinfo:VX|
+vti_author:SR|TOSHIBA\\FATIK
+vti_modifiedby:SR|TOSHIBA\\FATIK
+vti_nexttolasttimemodified:TW|09 Dec 2008 13:48:40 -0000
+vti_timecreated:TR|09 Dec 2008 15:00:16 -0000
+vti_cacheddtm:TX|09 Dec 2008 15:00:16 -0000
+vti_filesize:IR|20594
---
+++ en/_vti_cnf/indexYEDEK.php
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|09 Dec 2008 14:58:02 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{561E9537-9E40-44DC-BCF0-FF72B8A99D36}
+vti_backlinkinfo:VX|
+vti_author:SR|TOSHIBA\\FATIK
+vti_modifiedby:SR|TOSHIBA\\FATIK
+vti_nexttolasttimemodified:TW|09 Dec 2008 13:48:40 -0000
+vti_timecreated:TR|09 Dec 2008 14:58:02 -0000
+vti_cacheddtm:TX|09 Dec 2008 14:58:02 -0000
+vti_filesize:IR|20551
